0c3c066dbe2149d36f7a2ca25dfbee35365fe7b1
MsToT <2mstot@gmail.com>
2017-07-19 12:00:55 +0100

    changed start point after running with other links

--- pythonScripts/scrapeWho.py
+++ pythonScripts/scrapeWho.py
@@ -1,12 +1,12 @@
 # end results: getting all data from http://www.who.int/csr/don/archive/disease/ebola/en/ 
 
-import urllib.request
+from urllib import request
 
 p404 = 'The page or file you are trying to access cannot be found.' #will need this later for the loop
-link = 'http://www.who.int/csr/don/2001_12_12/en/'
+link = 'http://www.who.int/csr/don/2011_05_10/en/'
 filename = '2001Dec12.txt'
 
-with urllib.request.urlopen(link) as response:
+with request.urlopen(link) as response:
     html = str(response.read())
     if p404 not in html:
         with open(filename, 'w') as f:
@@ -14,17 +14,20 @@ with urllib.request.urlopen(link) as response:
 
 with open(filename, 'r') as f:
     page = f.read()
-    startP = page.find('Disease Outbreak Reported')
+    startP = page.find('<!-- begin: primary -->')
+    endP = page.find('<!-- end: primary -->')
     
-    # Narrow down the search span
-    txt = page[startP:page.find('<!-- end: primary -->')]
+    #Narrow down for better search
+    txt = page[startP:endP]
     sentences = txt.split('.')
 
-    keyTerms = ['deaths', 'suspected cases']
+    keyTerms = ['deaths', 'suspected cases', 'a case']
     cases = []
     
     #Get the reported cases from the text
     for s in sentences:
         if any(kt in s for kt in keyTerms):
             cases.append(s)
-    cases = [s for s in cases if 'WHO has received reports' in s]
+    
+    #Return only the relevant sentences (should be at least 1)
+    cases = [s for s in cases if 'WHO' in s]
